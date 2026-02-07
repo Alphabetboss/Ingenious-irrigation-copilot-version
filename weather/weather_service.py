c33bb474a615b9c90996ef9879a46100c095d80e
Austin Wilson <alphabetboss@outlook.com>
2026-02-07 01:41:02 -0600

    Update weather_service.py

--- weather/weather_service.py
+++ weather/weather_service.py
@@ -1,3 +1,11 @@
+class WeatherService:
+    def get_weather(self):
+        # Dummy weather data for demo purposes
+        return {
+            "rain_chance": 10,
+            "temperature": 75
+        }
+
 def should_skip_watering(weather):
     if weather["rain_chance"] > 60:
         return True
